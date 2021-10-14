2f3e37dcec0e28baa8c8044f48ec2bcb41d853dc
Daniel Lera <lera.danielm@gmail.com>
2021-10-14 09:22:03 -0300

    Crear componente para películas

---
+++ src/pelicula.js
@@ -0,0 +1,28 @@
+export default function Pelicula(props) {
+  return (
+    <div className="movie-item-style-2">
+      <img src={props.img} alt="" />
+      <div className="mv-item-infor">
+        <h6>
+          <a href="moviesingle.html">{props.titulo}</a>
+        </h6>
+        <p className="rate">
+          <i className="ion-android-star"></i>
+          <span>{props.puntaje}</span> /10
+        </p>
+        <p className="describe">{props.children}</p>
+        <p className="run-time">
+          {" "}
+          Run Time: {props.duracion}.<span>MMPA: {props.calificacion}.</span>{" "}
+          <span>Release: {props.fecha}.</span>
+        </p>
+        <p>
+          Director: <a href="#">{props.director}</a>
+        </p>
+        <p>
+          Stars: <a href="#">{props.actores}</a>
+        </p>
+      </div>
+    </div>
+  );
+}
