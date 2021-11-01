0303564530ed4e2f79add49d0e9618d2ad1ac567
Daniel Lera <lera.danielm@gmail.com>
2021-11-01 16:01:32 -0300

    Fix - Paginación

--- src/App.js
+++ src/App.js
@@ -2,13 +2,16 @@ import "./App.css";
 import Pelicula from "./Pelicula";
 import PageWrapper from "./PageWrapper";
 import Paginacion from "./Paginacion";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 
 function App() {
   const [paginaActual, setPaginaActual] = useState(1);
   const [peliculas, setPeliculas] = useState([]);
   const CANTIDAD_POR_PAGINA = 6;
 
+  useEffect(() => {
+    buscarPeliculas();
+  }, []);
   //Función asincrónica para consumir el archivo JSON
   const buscarPeliculas = async () => {
     let url = "https://api.jsonbin.io/b/618017739548541c29cbb4cf";
@@ -23,31 +26,25 @@ function App() {
     let json = await respuesta.json();
     setPeliculas(json);
   };
-  
-  const cargarPeliculas = () => {
-    peliculas = peliculas.slice(
-      (paginaActual - 1) * CANTIDAD_POR_PAGINA,
-      paginaActual * CANTIDAD_POR_PAGINA
-    );
-  };
 
   const getTotalPaginas = () => {
     return Math.ceil(peliculas.length / CANTIDAD_POR_PAGINA);
   };
 
-  //cargarPeliculas();
-  
+  let peliculasPorPagina = peliculas.slice(
+    (paginaActual - 1) * CANTIDAD_POR_PAGINA,
+    paginaActual * CANTIDAD_POR_PAGINA
+  );
 
   return (
     <PageWrapper>
-      <button onClick={buscarPeliculas}>Prueba</button>
       <Paginacion
         pagina={paginaActual}
         total={getTotalPaginas()}
         onChange={(pagina) => setPaginaActual(pagina)}
       />
 
-      {peliculas.map((pelicula) => {
+      {peliculasPorPagina.map((pelicula) => {
         return (
           <Pelicula
             img={pelicula.img}
